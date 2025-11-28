270b47850b8dae4658bd8b3c91b2e0a166aae87c
Hyojeong <shj000323@gmail.com>
2025-11-28 15:47:32 +0900

    routers 수정

--- server/src/routers/jwt_login.py
+++ server/src/routers/jwt_login.py
@@ -7,10 +7,10 @@ from fastapi import APIRouter, \
 from fastapi.responses import HTMLResponse, RedirectResponse # 해당 페이지로 바로 이동 시켜줌
 from sqlalchemy.orm import Session
 from src.database import get_db
-from models import User
+# from models import User
 import uuid
 from datetime import datetime
-from jwt_token import create_token, create_refresh_token, verify_token
+# from jwt_token import create_token, create_refresh_token, verify_token
 
 router = APIRouter(prefix='/jwt')
 
