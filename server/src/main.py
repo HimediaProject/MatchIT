ca7bce88e5d923133a6e1ff2e8128a97be7de1ae
Hyojeong <shj000323@gmail.com>
2025-12-01 17:03:31 +0900

    cors 추가

--- server/src/main.py
+++ server/src/main.py
@@ -8,7 +8,8 @@ app = FastAPI(title="MatchIT Backend")
 # 개발용 CORS 설정
 origins = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "http://frontend:3000"
 ]
 app.add_middleware(
     CORSMiddleware,
