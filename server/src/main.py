6fd6bdefb89693817f1d47765d48f9d8b79c045e
김재영 <kjy93177@gmail.com>
2025-12-02 18:04:02 +0900

    users.py 및 users_test.py 추가 및 router 추가

--- server/src/main.py
+++ server/src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import jwt_login, kakao, comparison
+from .routers import jwt_login, kakao, comparison, users, users_test
 
 app = FastAPI(title="MatchIT Backend")
 
@@ -22,6 +22,8 @@ app.add_middleware(
 app.include_router(jwt_login.router)
 app.include_router(kakao.router)
 app.include_router(comparison.router)
+app.include_router(users.router)
+app.include_router(users_test.router)
 
 @app.get("/")
 def root():
