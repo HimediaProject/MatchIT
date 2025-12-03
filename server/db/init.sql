084de2b1d6291780efcaf21389a05abbffbe0e91
seonayeon <sghnyyj@gmail.com>
2025-12-03 15:05:38 +0900

    models.py, schemas.py 수정 및 pgvector 추가

--- server/db/init.sql
+++ server/db/init.sql
@@ -1,6 +1,17 @@
 -- TimeZone 설정 (Asia/Seoul)
 ALTER DATABASE matchit_db SET TIMEZONE TO 'Asia/SEOUL';
 
+-- pgvector extension: Vector type for embeddings
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE TABLE IF NOT EXISTS test_result (
+    keyword     varchar(30) not null constraint test_result_tmp_pkey1 primary key,
+    embedding   vector(256) not null,
+    modified_at timestamp   not null
+);
+CREATE INDEX IF NOT EXISTS idx__test_result__embedding
+    ON test_result USING hnsw (embedding vector_cosine_ops);
+
 -- 경력 레벨
 CREATE TABLE CareerLevels (
     CareerLevelID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
