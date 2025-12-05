f6169eeba9ee7e4807d58230f1c2f306bf23e19b
leovis87 <parupin72@gmail.com>
2025-12-05 14:45:22 +0900

    bootcamper.py backend 기능 수정

--- server/src/main.py
+++ server/src/main.py
@@ -11,11 +11,14 @@ app = FastAPI(title="MatchIT Backend")
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://frontend:3000"
+    "http://frontend:3000",
+    "http://0.0.0.0:3000",
+    "http://backend:8000",
+    "http://localhost:5173"
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
