6d61473e9b8c91087cf763ad7831b670b23ec552
leovis87 <parupin72@gmail.com>
2025-12-05 14:45:22 +0900

    bootcamper.py backend 기능 수정

--- server/src/models.py
+++ server/src/models.py
@@ -134,6 +134,7 @@ class Platform(Base):
 # -------------------------------------------------------
 # JobCategories
 # -------------------------------------------------------
+# 이 값을 못가져옴....
 class JobCategory(Base):
     __tablename__ = "jobcategories"
 
