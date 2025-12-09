39e9ab27df6a9b6fea9214e3d1f064a81c119f6c
김재영 <kjy93177@gmail.com>
2025-12-09 17:51:53 +0900

    users.py 수정 및 프로필페이지 수정

--- server/src/routers/users.py
+++ server/src/routers/users.py
@@ -5,13 +5,13 @@ from fastapi import APIRouter, \
                     status, \
                     HTTPException
 from pydantic import BaseModel
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from typing import List, Optional, Dict, Any, cast, Union
 from src.database import get_db
 from src import models
 
 
-router = APIRouter(prefix="/users")
+router = APIRouter(prefix="/users", tags=['유저 프로필 기능'])
 
 # schemas 부분
 class ProfileOut(BaseModel):
@@ -32,18 +32,22 @@ class ProfileUpdate(BaseModel):
     career_level: Optional[Union[str, int]] = None
     skills: Optional[List[str]] = None
     desired_jobs: Optional[List[str]] = None
-    # employment_type: Optional[str] = None
-    # work_type: Optional[str] = None
 
+class JobPostOut(BaseModel):
+    id: int
+    title: Optional[str] = None
+    company_name: Optional[str] = None
 
-# class ProfileCreate(BaseModel):
-#     name: str
-#     career_level: Optional[Union[str, int]] = None
-#     skills: Optional[List[str]] = None
-#     desired_jobs: Optional[List[str]] = None
+    class Config:
+        orm_mode = True
 
-#     class Config:
-#         orm_mode = True
+class BootcampPostOut(BaseModel):
+    id: int
+    title: Optional[str] = None
+    institute_name: Optional[str] = None
+
+    class Config:
+        orm_mode = True
 
 class UserScrapGet(BaseModel):
     '''
@@ -61,7 +65,27 @@ class UserScrapGet(BaseModel):
     post_type: Optional[str] = None
     job_post_id: Optional[int] = None
     bootcamp_post_id: Optional[int] = None
+    job_post: Optional[JobPostOut] = None
+    bootcamp_post: Optional[BootcampPostOut] = None
 
+    class Config:
+        orm_mode = True
+
+class UserNotifications(BaseModel):
+    notification_type: Optional[str] = None
+    isenabled: Optional[bool] = None
+    notificationtime: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+class UserNotificationsUpdate(BaseModel):
+    notification_type: Optional[str] = None
+    isenabled: Optional[bool] = None
+    notificationtime: Optional[str] = None
+
+    class Config:
+        orm_mode = True
     
 def get_user_data(db: Session, user_id: int):
     user = (
@@ -76,16 +100,24 @@ def get_user_data(db: Session, user_id: int):
     return user
 
 def get_user_scrap(db: Session, user_id: int):
-    scrap = (
+    return (
         db.query(models.UserScrap)
+        .options(joinedload(models.UserScrap.job_post))
+        .options(joinedload(models.UserScrap.bootcamp_post))
         .filter(models.UserScrap.UserID == user_id)
-        .first()
+        .all()
     )
 
-    if not scrap:
-        raise HTTPException(status_code=404, detail="스크랩 항목을 찾을 수 없습니다.")
-    return scrap
+def get_user_notifications(db: Session, user_id: int):
+    notifications = (
+        db.query(models.UserNotificationSetting)
+        .filter(models.UserNotificationSetting.UserID == user_id)
+        .all()
+    )
 
+    if not notifications:
+        raise HTTPException(status_code=404, detail="알림 항목을 찾을 수 없습니다.")
+    return notifications
 
 @router.get("/{user_id}", response_model=ProfileOut)
 def read_profile(user_id: int, db: Session = Depends(get_db)):
@@ -201,94 +233,71 @@ def update_profile(user_id: int, data: ProfileUpdate, db: Session = Depends(get_
 
     return read_profile(user.UserID, db)
 
-# @router.post("/", response_model=ProfileOut, status_code=201)
-# def create_profile(data: ProfileCreate, db: Session = Depends(get_db)):
-#     user = models.User(Name=data.name)
-#     db.add(user)
 
-#     # 경력 레벨
-#     if data.career_level is not None:
-#         # 문자열인 경우
-#         if isinstance(data.career_level, str):
-#             career = db.query(models.CareerLevel).filter(
-#                 models.CareerLevel.CareerName == data.career_level
-#             ).first()
-#             if not career:
-#                 raise HTTPException(400, "존재하지 않는 커리어 레벨 이름입니다.")
-
-#             user.CareerLevelID = career.CareerLevelID
-
-#         # 숫자인 경우
-#         elif isinstance(data.career_level, int):
-#             career = db.query(models.CareerLevel).filter(
-#                 models.CareerLevel.CareerLevelID == data.career_level
-#             ).first()
-#             if not career:
-#                 raise HTTPException(400, "유효하지 않은 커리어 레벨 ID입니다.")
-
-#             user.CareerLevelID = data.career_level
-
-#     db.flush()
-
-#     # 스킬
-#     if data.skills is not None:
-#         new_skill_objs = []
-#         for name in data.skills:
-#             skill = db.query(models.Skill).filter(models.Skill.SkillName == name).first()
-#             if not skill:
-#                 skill = models.Skill(SkillName=name)
-#                 db.add(skill)
-#                 db.flush()
-#             new_skill_objs.append(skill)
-
-#         user.skills = new_skill_objs
-
-
-#     # 4) 희망직무
-#     if data.desired_jobs is not None:
-#         new_job_objs = []
-#         for name in data.desired_jobs:
-#             job = db.query(models.DesiredJob).filter(models.DesiredJob.JobName == name).first()
-#             if not job:
-#                 job = models.DesiredJob(JobName=name)
-#                 db.add(job)
-#                 db.flush()
-#             new_job_objs.append(job)
-
-#         user.desired_jobs = new_job_objs
-
-#     db.commit()
-#     db.refresh(user)
-
-#     return read_profile(user.UserID, db)
-
-
-
-# @router.delete("/{user_id}", status_code=204)
-# def delete_profile(user_id: int, db: Session = Depends(get_db)):
-#     user = get_user_data(db, user_id)
-#     if not user:
-#         raise HTTPException(status_code=404, detail="사용자를 찾을 수 없습니다.")
-    
-#     user.skills = []
-#     user.desired_jobs = []
-#     db.commit()
-
-#     db.delete(user)
-#     db.commit()
-
-#     return Response(status_code=204)
-
-@router.get("/{user_id}/scraps", response_model=UserScrapGet)
+@router.get("/{user_id}/scraps", response_model=List[UserScrapGet])
 def read_userscrap(user_id: int, db: Session = Depends(get_db)):
-    scrap = get_user_scrap(db, user_id)
+    scraps = get_user_scrap(db, user_id)
 
-    return UserScrapGet(
-        post_type=scrap.PostType,
-        job_post_id=scrap.JobPostID,
-        bootcamp_post_id=scrap.BootcampPostID,
-    )
+    result = []
 
+    for scrap in scraps:
+
+        if scrap.PostType == "Job" and scrap.job_post:
+            job_post = JobPostOut(
+                id=scrap.job_post.PostID,
+                title=scrap.job_post.Title,
+                company_name=scrap.job_post.CompanyName,
+            )
+        else:
+            job_post = None
+
+        if scrap.PostType == "Bootcamp" and scrap.bootcamp_post:
+            bootcamp_post = BootcampPostOut(
+                id=scrap.bootcamp_post.BootcampID,
+                title=scrap.bootcamp_post.Title,
+                institute_name=scrap.bootcamp_post.InstituteName,
+            )
+        else:
+            bootcamp_post = None
+
+        result.append(
+            UserScrapGet(
+                post_type=scrap.PostType,
+                job_post_id=scrap.JobPostID,
+                bootcamp_post_id=scrap.BootcampPostID,
+                job_post=job_post,
+                bootcamp_post=bootcamp_post
+            )
+        )
+    return result
+
+
+@router.get("/{user_id}/notifications", response_model=List[UserNotifications])
+def read_notifications(user_id: int, db: Session = Depends(get_db)):
+    notifications = get_user_notifications(db, user_id)
+
+    if not notifications:
+        raise HTTPException(404, "알림 항목을 찾을 수 없습니다.")
+    
+    return [
+        UserNotifications(
+            notification_type=n.NotificationType,
+            isenabled=n.IsEnabled,
+            notificationtime=n.NotificationTime,
+        )
+        for n in notifications
+    ]
+
+
+# @router.put("/{user_id}/notifications", response_model=UserNotificationsUpdate)
+# def update_notifications(user_id: int, db: Session = Depends(get_db)):
+#     notifications = get_user_notifications(db, user_id)
+
+#     return UserNotifications(
+#         notification_type=notifications.NotificationType,
+#         isenabled=notifications.IsEnabled,
+#         notificationtime=notifications.NotificationTime,
+#     )
 
 if __name__ == "__main__":
     import uvicorn
